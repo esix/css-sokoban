9af1adf157b807164d4d17f229af00975d99c12b
esix <esix@live.ru>
2021-01-15 01:01:53 +0300

    Iterative?

--- gen-level.js
+++ gen-level.js
@@ -382,14 +382,16 @@ function checkHasOne1(mask) {
  */
 function genLevel(level) {
   const states = genGraph(level);
+  const N = states.length;
 
   logGameStates(states);
   console.log(`Generated ${states.length} states`);
 
-  let connectedIdxs = [[]];
-  for (let i = 1; i < states.length; i++) {
+  // adjacency matrix
+  let connectedIdxs = [];
+  for (let i = 0; i < states.length; i++) {
     connectedIdxs[i] = [];
-    for (let j = 0; j < i; j++) {
+    for (let j = 0; j < states.length; j++) {
       if (states[j].up === states[i] || states[j].down === states[i] || states[j].left === states[i] || states[j].right === states[i] ||
           states[i].up === states[j] || states[i].down === states[j] || states[i].left === states[j] || states[i].right === states[j]) {
         connectedIdxs[i].push(j);
@@ -397,7 +399,316 @@ function genLevel(level) {
     }
   }
 
-  let results = states.map((state , i) => i ? -1 : 0);
+  function count1s(v) {
+    let result = 0;
+    for (; v; result++) v &= (v - 1n);
+    return result;
+  }
+
+  function make1(v, v1) {
+    let m = (v === -1) ? 1 :  (v ^ v1) << 1;                                        // setup to 1 first bit, next mask otherwise
+    if (count1s(m) !== 1) {
+      debugger;                                                         // mask must be 1
+    }
+    for (; v1 & m; m <<= 1);                                                   // search for 0 bit
+    return v1 | m;
+  }
+
+  function make2(v, v1, v2) {
+    let n = count1s(v1);
+    if (n !== count1s(v2)) { debugger; throw new Error(`v1 and v2 no match for bits: ${v1}, _${v2}_`); }
+    v = v1 | v2;
+    if (count1s(v) !== n + 1) {
+      // debugger;
+      return -1;
+    }
+    return v;
+  }
+
+  function make3(v, v1, v2, v3) {
+    let n = count1s(v1);
+    if (n !== count1s(v2)) { debugger; throw new Error(`v1, v2 and v3 no match for bits: ${v1}, _${v2}_, ${v3}`); }
+    if (n !== count1s(v3)) { debugger; throw new Error(`v1, v2 and v3 no match for bits: ${v1}, ${v2}, _${v3}_`); }
+    v = v1 | v2 | v3;
+    if (count1s(v) !== n + 1) {
+      // debugger;
+    }
+    return v;
+  }
+
+
+  // full distance matrix
+  let dists = states.map((_, i) => states.map((_, j) => i === j ? 0 : connectedIdxs[i].includes(j) ? 1 : Infinity));
+
+  for (let k = 0; k < N; k++) {
+    for (let i = 0; i < N; i++)
+      for (let j = 0; j < N; j++)
+        dists[i][j] = Math.min(dists[i][j], dists[i][k] + dists[k][j]);
+  }
+
+
+  let result = [0n];           // for the first one (depth=0) we set it
+
+  const abs = Math.abs;
+
+  debugger;
+  for (let depth = 1; ; depth++) {
+    let ss = states.filter(state => state.depth === depth);                   // states of depth
+    if (ss.length === 0) break;
+
+    // values for id
+    let parentValue = [];
+
+    // remove those that have more then one parent, find parents for those that are left, find result for them
+    ss = ss.filter(s => {
+      let sParentIds = connectedIdxs[s.id].filter(nid => states[nid].depth < depth);
+      if (sParentIds.length === 0) { debugger; throw new Error('NO PARENT FOR STATE')}
+      if (sParentIds.length === 1) { parentValue.push(result[sParentIds[0]]); return true; }
+      debugger;
+      // find the result for the state as OR function of its parents
+      let r = 0n;
+      sParentIds.forEach(pid => {
+        if (states[pid].depth !== depth - 1)  { debugger; throw new Error('Parent has invalid depth') }
+        r |= result[pid];
+      });
+      if (count1s(r) !== depth) { debugger; throw new Error('Invalid OR on parents gave wrong result')}
+      return false;                                                           // we don't need this element further
+    });
+
+    let w = ss.map(s1 => ss.map(s2 => dists[s1.id][s2.id]));                  // distance submatrix
+    console.log(w);
+
+    const d = (n1, n2) => count1s(n1 ^ n2);
+
+
+    function getNNumbersThatGivesSumEqualsK(n, k, fn) {
+      let r = [];
+      let rec = (i, k) => {
+        if (i === n - 1) {
+          r[i] = k;
+          return fn(r);
+        }
+        for (r[i] = 0; r[i] <= k; r[i] ++) {
+          if (rec(i + 1, k - r[i])) {
+            return true;
+          }
+        }
+      }
+      return rec(0, k);
+    }
+
+    debugger;
+    let maskSets = ss.map((s, i) => {
+      let maskSet = [];
+      let mask = 1n;
+      for (let j = 0; j < 100; j++) {
+        while (parentValue[i] & mask) mask <<= 1n;
+        maskSet.push(mask);
+        mask <<= 1n;
+      }
+      return maskSet;
+    });
+
+
+    let r = [];
+
+    for (let sum = 0; sum < 100; sum++) {
+      console.log('sum=', sum);
+      let success = getNNumbersThatGivesSumEqualsK(maskSets.length, sum, (maskIndexes) => {
+        let masks = maskIndexes.map((maskIndex, i) => maskSets[i][maskIndex]);
+        r = masks.map((mask, i) => parentValue[i] ^ mask);
+        // check r...
+        for (let i = 0; i < r.length; i++) {
+          for (let j = 0; j < r.length; j++) {
+            if (d(r[i], r[j]) !== w[i][j]) return false;
+          }
+        }
+        return true;
+      });
+      if (success) break;
+    }
+
+    debugger;
+
+
+    // let r = [];                                                               // local result
+    //
+    // let rec = (i) => {
+    //   if (i === ss.length) return true;
+    //   const p = parentValue[i];
+    //   outer:
+    //   for (let bit = 0n; bit < 30n; bit++) {
+    //     let mask = 1n << bit;
+    //     if (!(p & mask)) {                                           // has corresponding bit set to 0
+    //       r[i] = p ^ mask;                                           // set this bit to 1
+    //       for (let j = 0; j < i; j++) {
+    //         if (d(r[i], r[j]) !== w[i][j]) continue outer;      // not match binary distance => try next bit
+    //       }
+    //
+    //       if (rec(i+1)) {
+    //         return true;
+    //       }
+    //     }
+    //   }
+    // }
+    //
+    // debugger;
+    // let res = rec(0);
+    // if (!res) {
+    //   debugger;
+    // }
+
+
+
+
+
+    // let countError = (chromosome) => {
+    //   let error = 0;
+    //   ss.forEach((s, i) => error += 10000 * abs(count1s(chromosome[i]) - depth));      // number or 1 bits should be equal to depth
+    //   ss.forEach((s1 , i) => ss.forEach((s2, j) => {                  // should be uniq
+    //     if (i !== j && chromosome[i] === chromosome[j]) error += 1000;
+    //   }));
+    //   ss.forEach((s, i) => error += 100 * abs(count1s(chromosome[i] ^ parentValue[i]) - 1)); // parent should differ only one bit
+    //
+    //   ss.forEach((s1 , i) => ss.forEach((s2, j) => {                  // should match distances
+    //     if (i !== j)
+    //       error += abs(1 * count1s(chromosome[i] ^ chromosome[j]) - w[i][j]);                             //
+    //   }));
+    //
+    //   return error;
+    // }
+    //
+
+
+
+
+
+
+
+    let mutate = (chromosome) => {
+      let idx = Math.floor(Math.random() * chromosome.length);
+      let c = chromosome[idx]
+      let len = c.toString(2).length;
+      if (Math.random() > 0.8) {
+        let bitIdx = BigInt(Math.floor(Math.random() * (len + 1)));
+        c = c ^ (1n << bitIdx);
+      } else {  // swap two random bits
+        let i1 = BigInt(Math.floor(Math.random() * (len + 2)));
+        let i2 = BigInt(Math.floor(Math.random() * (len + 2)));
+        let b1 = (c & (1n << i1)) >> i1;
+        let b2 = (c & (1n << i2)) >> i2;
+        if (b1) c |= (1n << i2); else c ^= (1n << i2);
+        if (b2) c |= (1n << i1); else c ^= (1n << i1);
+      }
+      chromosome[idx] = c;
+      return chromosome;
+    }
+
+    let crossover = (chromosome1, chromosome2) => {
+      let n = chromosome1.length;
+      let result = [];
+      for (let i = 0; i < n; i++) {
+        let v1 = chromosome1[i], v2 = chromosome2[i];
+        let diffBits = (v1 ^ v2).toString(2);
+        let or = 0n;
+        for (let i = 0; i < diffBits.length; i++) {
+          if (diffBits[i] === '1' && Math.random() < 0.5) {
+            or |= (1n << BigInt(i));                                                                    // setup some of diff bits to 1
+          }
+        }
+        result.push(v1 | or);
+      }
+      return result;
+    }
+
+    // let chromosomes = [
+    //     ss.map((s, i) => 1n << BigInt(i)),
+    //     ss.map((s, i) => 1n << BigInt(i + 1)),
+    //     ss.map((s, i) => 1n << BigInt(ss.length - i - 1)),
+    //     ss.map((s, i) => 1n << BigInt(ss.length - i))];
+
+    // run genetic
+    // let localResult = null;
+    //
+    // for (let g = 0; g < 10000; g++) {
+    //   let n = chromosomes.length;
+    //
+    //   let newChromosomes = {};
+    //   chromosomes.forEach(c => newChromosomes[c] = c);
+    //
+    //   for (let i = 0; i < n; i++) {
+    //     for (let j = 0; j < n; j++) {
+    //       let c = mutate(crossover(chromosomes[i], chromosomes[j]));
+    //       newChromosomes[c] = c;
+    //     }
+    //   }
+    //   chromosomes = Object.values(newChromosomes);
+    //
+    //   let pairs = chromosomes.map(chromosome => [countError(chromosome), chromosome]);
+    //   pairs.sort(([e1, c1], [e2, c2]) => e1 - e2);
+    //   pairs = pairs.slice(0, 10);
+    //   let bestPair = pairs[0];
+    //   console.log(bestPair[0]);
+    //   if (bestPair[0] === 0) {                                                // error === 0
+    //     localResult = bestPair[1];
+    //     break;
+    //   }
+    //   chromosomes = pairs.map(([v, c]) => c);
+    // }
+    //
+    //
+    // if (!localResult) {
+    //   debugger;
+    //   let c = chromosomes[0];
+    //   let maxLen = Math.max.apply(Math, c.map(c => c.toString(2).length));
+    //   console.log(countError(c), c, '\n' + c.map(c => c.toString(2).padStart(maxLen, '0')).join('\n'));
+    //   throw new Error('No result found');
+    // }
+
+    ss.forEach((s, i) => result[s.id] = r[i]);
+  }
+
+
+
+  /*
+  let results = states.map(() => -1);
+  results[0] = 0;
+
+  for (let i = 1; i < states.length; ) {
+    let idxs = connectedIdxs[i].filter(j => j < i);
+    let connectedValues = idxs.map(j => results[j]);
+    if (connectedValues.length === 0) throw new Error('0');
+    else if (connectedValues.length === 1) {
+      results[i] = make1(results[i], connectedValues[0], used);
+      if (used[results[i]]) continue;
+      used[results[i]] = true;
+    } else if (connectedValues.length === 2) {
+      results[i] = make2(results[i], connectedValues[0], connectedValues[1]);
+      if (results[i] === -1 || used[results[i]]) {                                                    // fallback to idxs[1]
+        for (; i > idxs[1]; i--) { used[results[i]] = false; results[i] = -1; }
+        continue;
+      }
+      used[results[i]] = true;
+
+    } else if (connectedValues.length === 3) {
+      results[i] = make3(results[i], connectedValues[0], connectedValues[1], connectedValues[2]);
+      if (results[i] === -1 || used[results[i]]) {                                                    // fallback to idxs[1]
+        for (; i > idxs[1]; i--) { used[results[i]] = false; results[i] = -1; }
+        continue;
+      }
+      used[results[i]] = true;
+
+    } else if (connectedValues.length === 4) {
+      throw new Error('4');
+    } else {
+      throw new Error('MANY???');
+    }
+    console.log(`state ${i} (${states[i].depth}) connected`, connectedValues,`make value  = ${results[i]} (${results[i].toString(2).padStart(8, '0')})`);
+
+    i++;
+  }
+
+
 
   let i = 1;
 
@@ -453,38 +764,8 @@ function genLevel(level) {
   }
 
   console.log(results.join(','));
+  */
 
-  //
-  // const maxDepth = Math.max.apply(Math, states.map(state => state.depth));
-  //
-  // const chromosome0 = states.map((_, i) => i).join(',');
-  // const chromosome1 = states.map((_, i) => 2 * i).join(',');
-  // let population = [chromosome0, chromosome1];
-  //
-  // for (let depth = 0; depth <= maxDepth; depth++) {
-  //   console.log('depth=', depth);
-  //   let score = 0, n = 0;
-  //   do {
-  //     population = populationStep(states, population, depth);
-  //     score = countFitness(states, population[0], depth);
-  //
-  //     if (++n % 100 === 0) {
-  //       console.log('iteration', n, 'score=', score, 'chromosome=', population[0].split(',').map((gene, i) => states[i].depth <= depth ? gene : '_' ).join(','));
-  //     }
-  //
-  //     if (n > 1000) {
-  //       console.log('REPLAY depth = ', depth, 'score=', score, 'chromosome=', population[0].split(',').map((gene, i) => states[i].depth <= depth ? gene : '_' ).join(','));
-  //       depth -= 2;
-  //       break;
-  //     }
-  //   } while (score > 0);
-  //
-  //   if (score === 0) {
-  //     console.log('SUCCESS depth = ', depth, 'score=', score, 'chromosome=', population[0].split(',').map((gene, i) => states[i].depth <= depth ? gene : '_' ).join(','));
-  //   }
-  // }
-  //
-  // console.log(population[0]);
 }
 
 
